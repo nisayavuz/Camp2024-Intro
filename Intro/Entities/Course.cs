286677a1ec84aaf3699326084e652459b54b014d
Nisa Yavuz <nsnryvz@outlook.com>
2024-03-16 23:26:19 +0300

    Add project files.

---
+++ Intro/Entities/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Entities;
+
+//Entities --> veri tutucu sınıflar , Busines --> operasyon tutucu
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,37 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+
+
+//Course course1 = new Course();
+//course1.Id = 1;
+//course1.Name = "C#";
+//course1.Description = ".NET 8";
+//course1.Price = 0;
+
+//Course course2 = new Course();
+//course2.Id = 2;
+//course2.Name = "Java";
+//course2.Description = "JAVA 17";
+//course2.Price = 10;
+
+//Course course3 = new Course();    
+//course3.Id = 3;
+//course3.Name = "Python";
+//course3.Description = "Python 3.12";
+//course3.Price = 20;
+
+//Course[] courses = { course1, course2, course3 };
+
+//for (int i = 0; i < courses.Length; i++)
+//{
+//    Console.WriteLine(courses[i].Name + " / " + courses[i].Description);
+//}
+
+CourseManager courseManager = new(); //yeni syntax ile bu şekilde newleyebiliyoruz.
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
